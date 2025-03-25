043a9a7b26fce7f9528b0e557bf05cb6cb2ba7d3
Kenji Ozawa <keoz002@ess.ais.ac.nz>
2025-03-26 12:27:40 +1300

    create the login view, and fixed the problem with registration and authentication

--- note_app/serializers.py
+++ note_app/serializers.py
@@ -6,3 +6,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        user.set_password(validated_data['password'])
+        return user
