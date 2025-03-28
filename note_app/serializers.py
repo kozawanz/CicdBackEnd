9a209e8b935438361d49d59b397b7873c958f94c
kozawanz <keoz002@ess.ais.ac.nz>
2025-03-29 00:06:58 +1300

    Revert "Create note implementation"

--- note_app/serializers.py
+++ note_app/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Note
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,8 +11,3 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         user.set_password(validated_data['password'])
         return user
-
-
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        '__all__'
